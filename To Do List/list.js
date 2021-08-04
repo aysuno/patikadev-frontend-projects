5f5983b2b64f9ce266b7a44a25ca44d608156488
Aysun <aysuno@sabanciuniv.edu>
2021-08-04 14:05:13 +0300

    app completed

--- To Do List/list.js
+++ To Do List/list.js
@@ -1,8 +1,34 @@
-
 var list = document.querySelector("ul");
 var add = document.querySelector("#addButton");
+var nodeList =document.getElementsByTagName("li");
+var deleteItem = document.getElementsByClassName("close");
 
+//Adding X (closing) to *existing* list items
+for(var i= 0; i < nodeList.length; i++){
+    var span = document.createElement('span');
+    var text = document.createTextNode('\u00D7'); // X char in ascii
+    span.className = 'close';
+    span.appendChild(text);
+    nodeList[i].appendChild(span);
+}
 
+//Deleting Items 
+for (var i = 0; i < deleteItem.length; i++) {
+  deleteItem[i].onclick = function () {
+    var selected = this.parentElement;
+    selected.style.display = "none";
+  };
+}
+
+//If task done --> line through
+list.addEventListener("click",function (item) {
+    if (item.target.tagName === "LI") {
+      item.target.classList.toggle("checked");
+    }
+  }
+);
+
+//Adding Items
 add.addEventListener("click",function(){
     if(textHere.value == ""){
         alert("You must type something to add item to the list");
@@ -11,8 +37,22 @@ add.addEventListener("click",function(){
         var li=document.createElement("li"); 
         li.textContent=textHere.value;  
         list.appendChild(li);
+        var span = document.createElement('span');
+        var text = document.createTextNode('\u00D7'); // X char in ascii
+        span.className = 'close';
+        span.appendChild(text);
+        li.appendChild(span);
         textHere.value = "";
-    }
 
+        //Deleting Items 
+        for (var i = 0; i < deleteItem.length; i++) {
+            deleteItem[i].onclick = function () {
+            var selected = this.parentElement;
+            selected.style.display = "none";
+            };
+        }
+    }
 });
+
+
 
